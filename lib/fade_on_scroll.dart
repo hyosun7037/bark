9472f41656c70f5fd729c85e092022f4b4d466e5
hyosun7037 <>
2022-07-15 16:46:07 +0900

    [MOD] 메인 슬라이드 다른방식 적용중

--- lib/fade_on_scroll.dart
+++ lib/fade_on_scroll.dart
@@ -1,14 +1,11 @@
 import 'package:barktest/controller/appbar_controller.dart';
-import 'package:barktest/item_list.dart';
-import 'package:barktest/sliding_cards_view.dart';
-import 'package:card_swiper/card_swiper.dart';
+import 'package:carousel_images/carousel_images.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 
 class FadeOnScroll extends GetView<AppbarController> {
+  @override
   final controller = Get.put(AppbarController());
   late AnimationController animationController;
   PageController pageController = PageController(initialPage: 0);
@@ -21,18 +18,7 @@ class FadeOnScroll extends GetView<AppbarController> {
   Widget build(BuildContext context) {
     scroll.addListener(() {
       controller.getColor(scroll.position.pixels);
-      void didChangeDependencies() {
-        precacheImage(Image.asset('assets/meta.jpg').image, context);
-        precacheImage(Image.asset('assets/test01.jpg').image, context);
-        precacheImage(Image.asset('assets/test02.jpg').image, context);
-        precacheImage(Image.asset('assets/test03.jpg').image, context);
-        precacheImage(Image.asset('assets/test04.jpg').image, context);
-      }
     });
-    assetImageList = [
-      'assets/test04.jpg',
-      'assets/test05.jpg',
-    ];
     return Scaffold(
       body: CustomScrollView(
         controller: scroll,
@@ -79,8 +65,19 @@ class FadeOnScroll extends GetView<AppbarController> {
               floating: false,
               pinned: false,
               flexibleSpace: FlexibleSpaceBar(
-                background: CarouselSlider(
-                    items: [
+                background:
+                    //  CarouselImages(
+                    //   viewportFraction: 1.0,
+                    //   borderRadius: 0,
+                    //   cachedNetworkImage: true,
+                    //   listImages: [
+                    //     'assets/test04.jpg',
+                    //     'assets/test05.jpg',
+                    //   ],
+                    //   height: 500,
+                    // )
+                    CarouselSlider(
+                        items: [
                       Image.asset(
                         'assets/test04.jpg',
                         fit: BoxFit.cover,
@@ -90,70 +87,32 @@ class FadeOnScroll extends GetView<AppbarController> {
                         fit: BoxFit.cover,
                       ),
                     ],
-                    options: CarouselOptions(
-                        pauseAutoPlayInFiniteScroll: true,
-                        autoPlayInterval: Duration(seconds: 3),
-                        autoPlayAnimationDuration: Duration(milliseconds: 600),
-                        initialPage: 1,
-                        autoPlay: true,
-                        viewportFraction: 1,
-                        height: 500)),
-                //     Swiper(
-                //   curve: Curves.easeInOut,
-                //   autoplay: true,
-                //   axisDirection: flipAxisDirection(AxisDirection.left),
-                //   itemWidth: MediaQuery.of(context).size.width,
-                //   itemHeight: 600,
-                //   // layout: SwiperLayout.STACK,
-                //   itemCount: assetImageList.length,
-                //   autoplayDisableOnInteraction: false,
-                //   itemBuilder: (context, index) {
-                //     final image = assetImageList[index];
-                //     return Image(
-                //       image: AssetImage(image),
-                //       fit: BoxFit.cover,
-                //     );
-                //     // return SlidingCard(
-                //     //   name: '',
-                //     //   date: '',
-                //     //   image: assetImageList[index],
-                //     //   offset: pageOffset,
-                //     // );
-                //   },
-                // ),
-              )
-              // FlexibleSpaceBar(
-              //     background: PageView.builder(
-              //         pageSnapping: true,
-              //         controller: pageController,
-              //         itemCount: demo.length,
-              //         onPageChanged: (value) {},
-              //         itemBuilder: (context, index) {
-              //           return SlidingCard(
-              //             name: '브랜드 즐겨찾기$index',
-              //             date: '4.20-30',
-              //             assetName: demo[index],
-              //             offset: pageOffset,
-              //           );
-              //         })),
-              )),
+                        options: CarouselOptions(
+                            pauseAutoPlayInFiniteScroll: true,
+                            autoPlayInterval: Duration(seconds: 3),
+                            autoPlayAnimationDuration:
+                                Duration(milliseconds: 600),
+                            initialPage: 1,
+                            autoPlay: true,
+                            viewportFraction: 1,
+                            height: 500)),
+              ))),
           SliverFillRemaining(
             fillOverscroll: true,
             child: SafeArea(
+              minimum: EdgeInsets.only(top: 40),
               child: Container(
-                padding: EdgeInsets.only(top: 20),
                 child: Column(
                   children: [
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('우먼'),
-                        Text('맨'),
-                        Text('라이프'),
-                        Text('선물'),
-                        Text('베스트'),
-                        Text('추천'),
+                      children: const [
+                        Text('사료'),
+                        Text('간식'),
+                        Text('장난감'),
+                        Text('산책용품'),
+                        Text('목욕')
                       ],
                     ),
                   ],
